fc1ced32257bbd3f0b353bfe426e607973b73544
engelsgermosen <mrengels2648@gmail.com>
2025-01-03 02:02:40 -0400

    01

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {animation: {
+			fadeIn: 'fadeIn 0.3s ease-in-out',
+			fadeOut: 'fadeOut 0.5s ease-in-out',
+		  },
+		  keyframes: {
+			fadeIn: {
+			  '0%': { opacity: '0' },
+			  '100%': { opacity: '1' },
+			},
+			fadeOut: {
+			  '0%': { opacity: '1' },
+			  '100%': { opacity: '0' },
+		  }}},
 	},
 	plugins: [],
 }
